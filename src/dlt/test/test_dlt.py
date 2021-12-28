033c7f920f22561bab97662225f51218ae87a69f
Jared Magrath <magrathj@tcd.ie>
2021-12-28 19:27:58 +0000

    added unit test to dlt_read

--- src/dlt/test/test_dlt.py
+++ src/dlt/test/test_dlt.py
@@ -32,3 +32,35 @@ def test_dlt_table(spark):
     assert read_table().count() == 5
     assert spark.read.table("read_table").count() == 5
 
+def test_dlt_read(spark):
+    """test dlt.read() function"""
+
+    data = [
+        ("James","","Smith","36636","M",3000),
+        ("Michael","Rose","","40288","M",4000),
+        ("Robert","","Williams","42114","M",4000),
+        ("Maria","Anne","Jones","39192","F",4000),
+        ("Jen","Mary","Brown","","F",-1)
+    ]
+
+    schema = StructType([ \
+        StructField("firstname",StringType(),True), \
+        StructField("middlename",StringType(),True), \
+        StructField("lastname",StringType(),True), \
+        StructField("id", StringType(), True), \
+        StructField("gender", StringType(), True), \
+        StructField("salary", IntegerType(), True) \
+    ])
+
+    # create temp view
+    (
+        spark
+        .createDataFrame(data=data, schema=schema)
+        .createOrReplaceTempView("test_dataframe")
+    )
+
+    df = dlt.read(
+        "test_dataframe"
+    )
+    assert df.count() == 5
+
