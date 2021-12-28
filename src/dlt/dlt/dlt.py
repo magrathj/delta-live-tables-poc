3745c93d0cfeb95516497e13d1271a63ca4638e8
Jared Magrath <magrathj@tcd.ie>
2021-12-28 20:11:47 +0000

    add view func and test

--- src/dlt/dlt/dlt.py
+++ src/dlt/dlt/dlt.py
@@ -24,8 +24,21 @@ class dlt:
                 called_fun.createOrReplaceTempView(delta_live_table)
                 print(f"created table called: {delta_live_table}")
                 return called_fun                        
-            return decorated 
-            
+            return decorated             
+        return decorator
+
+    def view(*outer_args,**outer_kwargs):                            
+        def decorator(fn):                                            
+            def decorated(*args,**kwargs): 
+                delta_live_table = fn.__name__
+                print(f"arguments:{args}, key word arguments:{kwargs}")
+                print(f"calling table decorator")
+                called_fun = fn(*args,**kwargs)
+                print(f"creating table called: {delta_live_table}")
+                called_fun.createOrReplaceTempView(delta_live_table)
+                print(f"created table called: {delta_live_table}")
+                return called_fun                        
+            return decorated             
         return decorator
     
     def expect(*outer_args,**outer_kwargs):                            
--- src/dlt/test/test_dlt.py
+++ src/dlt/test/test_dlt.py
@@ -32,6 +32,38 @@ def test_dlt_table(spark):
     assert read_table().count() == 5
     assert spark.read.table("read_table").count() == 5
 
+def test_dlt_view(spark):
+    """test @dlt.view() function"""
+
+    data = [
+        ("James","","Smith","36636","M",3000),
+        ("Michael","Rose","","40288","M",4000),
+        ("Robert","","Williams","42114","M",4000),
+        ("Maria","Anne","Jones","39192","F",4000),
+        ("Jen","Mary","Brown","","F",-1)
+    ]
+
+    schema = StructType([ \
+        StructField("firstname",StringType(),True), \
+        StructField("middlename",StringType(),True), \
+        StructField("lastname",StringType(),True), \
+        StructField("id", StringType(), True), \
+        StructField("gender", StringType(), True), \
+        StructField("salary", IntegerType(), True) \
+    ])
+
+    @dlt.view(
+        comment="Test"
+    )
+    def read_table():
+        return (
+            spark.createDataFrame(data=data, schema=schema)
+        )
+
+    assert read_table().count() == 5
+    assert spark.read.table("read_table").count() == 5
+
+
 def test_dlt_read(spark):
     """test dlt.read() function"""
 
